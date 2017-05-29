a26bc2b94790f22f54e5075ea01020999fcb9aef
Andres Kwan <andreskwan@gmail.com>
2017-05-29 12:35:35 -0500

    001c [VC add tvDataSource required methods]

--- 001-Challenge-TableOfBugs/ViewController.swift
+++ 001-Challenge-TableOfBugs/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var bugs = [ScaryBug]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        bugs = ScaryBug.bugs()
+        automaticallyAdjustsScrollViewInsets = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +27,22 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController : UITableViewDataSource {
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bugs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //obtain reusable cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BugCell", for: indexPath)
+        //obtain data to be placed in the cell
+        let bug = bugs[indexPath.row]
+        cell.textLabel?.text = bug.name
+        cell.detailTextLabel?.text = ScaryBug.scaryFactorToString(scaryFactor: bug.howScary)
+        if let imageView = cell.imageView, let bugImage = bug.image {
+            imageView.image = bugImage
+        }
+        return cell
+    }
+}
